b0a68ed8981d09c26fd134e46bec2399cb4e4bda
williambcala <william.banguera@uao.edu.co>
2023-05-24 21:54:57 -0500

    challenge18

--- controllers/Task.js
+++ controllers/Task.js
@@ -1,3 +1,5 @@
+const usuario = require("../models/usuario");
+
 const crearTask = async (req,res = express.request)=>{
     const task = new Task(req.body);
 
@@ -8,6 +10,24 @@ const crearTask = async (req,res = express.request)=>{
         res.json({
             ok:true,
             task:saved
+        })
+    }catch(error){
+        console.log(error);
+        res.status(500).json({
+            ok:false,
+            task: 'Internal error'
+        })
+    }
+}
+
+const listarTasks = async (req,res = express.request)=>{
+    const usuarios = await usuario.find()
+    .populate('tareas','title');
+
+    try{
+        res.status(200).json({
+            ok:true,
+            usuarios
         })
     }catch(error){
         console.log(error);
--- models/TaskScheme.js
+++ models/TaskScheme.js
@@ -19,4 +19,109 @@ TaskScheme.method('toJSON', function(){
 
 })
 
+app.put('/tasks/:id', (req, res) => {
+    const taskId = req.params.id;
+    const updatedTaskData = req.body; 
+  
+    Task.findByIdAndUpdate(taskId, updatedTaskData, { new: true }, (err, updatedTask) => {
+      if (err) {
+      
+        console.error(err);
+        res.status(500).json({ error: 'internal error' });
+      } else if (!updatedTask) {
+        
+        res.status(404).json({ error: 'internal error' });
+      } else {
+        
+        res.json(updatedTask);
+      }
+    });
+  });
+  
+  
+  app.delete('/tasks/:id', (req, res) => {
+    const taskId = req.params.id;
+  
+    Task.findByIdAndDelete(taskId, (err, deletedTask) => {
+      if (err) {
+        
+        console.error(err);
+        res.status(500).json({ error: '' });
+      } else if (!deletedTask) {
+       
+        res.status(404).json({ error: '' });
+      } else {
+        
+        res.json({ message: '' });
+      }
+    });
+  });
+  
+  
+  const mongoose = require('mongoose');
+  
+  taskSchema = new mongoose.Schema({
+  
+    title: String,
+    description: String,
+  
+  });
+  
+Task = mongoose.model('Task', taskSchema);
+  
+  module.exports = Task;
+  
+ Task = require('./controllers/Task');
+  
+  
+  Task.findById(taskId, (err, task) => {
+    if (err) {
+  
+      console.error(err);
+    } else if (!task) {
+   
+      console.log('error',err);
+    } else {
+    
+      console.log(task);
+    }
+  });
+
+
+Task = require('./controllers/Task');
+
+
+Task.findById(taskId, (err, task) => {
+  if (err) {
+    console.error(err);
+  } else if (!task) {
+    console.log('Tarea no disponible');
+  } else if (task.userId !== userId) {
+    
+    console.log('usuario no autorizado');
+  } else {
+    
+    console.log(task);
+  }
+});
+
+const Task = require('./models/task');
+
+
+put('/tasks/:id', (req, res) => {
+const taskId = req.params.id;
+const updatedTaskData = req.body;
+
+Task.findByIdAndUpdate(taskId, updatedTaskData, { new: true }, (err, updatedTask) => {
+    if (err) {
+        console.error(err);
+       
+status(500).json({ error: '' });
+    } else if (!updatedTask) {
+      res.status(404).json({ error: '' });
+    }else {res.json(updatedTask);
+    }
+  });
+});
+
 module.exports = model('Task',TaskScheme);
